d93025e5fdeb1aaa91324073b9d22fcd274d3f5f
Nelson Da Rocha Dias <nda-roch@c2r2s10.42porto.com>
2026-09-08 16:17:45 +0100

    started main

--- main.py
+++ main.py
@@ -7,6 +7,23 @@
 #   By: nda-roch <nda-roch@student.42porto.com>      +#+  +:+       +#+       #
 #                                                  +#+#+#+#+#+   +#+          #
 #   Created: 2026/08/27 14:38:52 by nda-roch            #+#    #+#            #
-#   Updated: 2026/08/27 14:38:53 by nda-roch           ###   ########.fr      #
+#   Updated: 2026/09/08 16:16:30 by nda-roch           ###   ########.fr      #
 #                                                                             #
 # ########################################################################### #
+
+from parser import Parser
+from exceptions import FlyInError
+from sys import argv, exit
+
+
+def main() -> None:
+    try:
+        parsed_map = Parser().parse(argv[1])
+
+    except FlyInError as e:
+        print(f"Error: {e}")
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
